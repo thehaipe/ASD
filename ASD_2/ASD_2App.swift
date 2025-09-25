36456bccc727023e3687bfc024f4ff4b42e2e6f7
thehaipe <thehaipe2007@gmail.com>
2025-09-25 15:26:31 +0300

    Initial Commit

---
+++ ASD_2/ASD_2App.swift
@@ -0,0 +1,17 @@
+//
+//  ASD_2App.swift
+//  ASD_2
+//
+//  Created by Valentyn on 25.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ASD_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
